16a7940546e2d16860ac3ad41af009f1158c9ed4
ydove0324 <1352035939@qq.com>
2025-03-02 15:37:42 +0800

    init commit

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base: '/imaginarum',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
